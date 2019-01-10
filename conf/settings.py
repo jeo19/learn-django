101c9751631f2de0c056df434194da4bf03eace8
nila <terrakkk@outlook.com>
2019-01-11 01:35:49 +0630

    Changed 'DATABASES' setting to postgreSQL on the settings.py

--- conf/settings.py
+++ conf/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'conf.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django_test',
+        'USER': 'django_user',
+        'PASSWORD': 'django_pass',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
